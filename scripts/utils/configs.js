fcaeaebd11e01af9e160cd40125076ac41997329
Jorge Luiz Chaves de Oliveira <jorge.oliveira@easynvest.com.br>
2017-10-20 14:50:14 -0200

    changed simulador calc endpoint

--- scripts/utils/configs.js
+++ scripts/utils/configs.js
@@ -1,7 +1,7 @@
 (
     function() {
         simulator.configs = {
-            URL_API_SIMULATOR: 'https://easynvestsimulatorcalcapi.azurewebsites.net/calculator/simulate'
+            URL_API_SIMULATOR: 'http://api-simulator-calc.us-east-1.elasticbeanstalk.com//calculator/simulate'
         }
     }
 )()
